8af7b2dda41d4179185e7c63c718483cd6815e0b
Shubham Saxena <shubh.lpu16@gmail.com>
2020-09-22 17:25:02 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ContextProvider } from './AppContext';
+import {Provider as ReduxProvider} from 'react-redux';
+import store from './store/index';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+  <ReduxProvider store={store}>
+    <ContextProvider value={{ store }}>
+      <Router>
+        <App />
+      </Router>
+    </ContextProvider>
+  </ReduxProvider>,
+  document.getElementById('root'),
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
